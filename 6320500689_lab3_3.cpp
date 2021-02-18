4e3d6e0b10bf7742e4c043f8acde3e3c035f0800
b6320500689 <supitchaya.r@ku.th>
2021-02-18 15:15:18 +0700

    Factorization

---
+++ 6320500689_lab3_3.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    unsigned int k=1,i;
+	if(k>=1 && k<=1000)
+		scanf("%d",&k);
+		
+	if(k == 1)
+		printf("1\n");
+		
+    for(i=2; k!=1; i++)
+    {
+        while(k%i == 0)
+        {
+            k = k/i;
+            printf("%d\n",i);
+            
+            if(k/i == 0)
+                printf("%d",k/i);
+        }
+    }
+    return 0;
+}
